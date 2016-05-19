34d356333779f7ae0ca9f2a723495c7e3252ac40
Deborah Engelmeyer <dengelmeyer@icloud.com>
2016-05-18 20:59:51 -0700

    Added CollectionViewController for project page.  Also set it up to have 9 cells with 3x3 columns.  Unable to use derived type for cell view.

---
+++ YarnRequirements/ProjectCell.swift
@@ -0,0 +1,26 @@
+//
+//  ProjectCell.swift
+//  YarnRequirements
+//
+//  Created by Deb on 5/18/16.
+//  Copyright © 2016 The Inquisitive Introvert. All rights reserved.
+//
+
+import UIKit
+
+class ProjectCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+    
+//    override func setSelected(selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//        
+//        // Configure the view for the selected state
+//    }
+}
---
+++ YarnRequirements/ProjectController.swift
@@ -0,0 +1,101 @@
+//
+//  ProjectController.swift
+//  YarnRequirements
+//
+//  Created by Deb on 5/18/16.
+//  Copyright © 2016 The Inquisitive Introvert. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "projectCell"
+
+class ProjectController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        let layout = self.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.estimatedItemSize = CGSizeMake(108, 117.0);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 9
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) //as! ProjectCell
+
+        // Configure the cell
+        cell.backgroundColor = UIColor.brownColor()
+        //cell.lblName.text = "Name"
+    
+        return cell
+    }
+
+    func collectionView(collectionView : UICollectionView, layout collectionViewLayout:UICollectionViewLayout, sizeForItemAtIndexPath indexPath:NSIndexPath) -> CGSize
+    {
+        let cellSize:CGSize = CGSizeMake(108, 117)
+        return cellSize
+    }
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
