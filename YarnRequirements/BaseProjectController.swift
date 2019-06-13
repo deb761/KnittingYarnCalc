a4c832724904727f4a64fd5ada8e44098929449e
Deborah Engelmeyer <dengelmeyer@icloud.com>
2019-06-13 08:32:17 -0700

    Set the units text field to show a picker
    Have the BaseProjectController provide a protocol to update the project

--- YarnRequirements/BaseProjectController.swift
+++ YarnRequirements/BaseProjectController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BaseProjectController: UITableViewController, UIPickerViewDelegate, UITextFieldDelegate {
+class BaseProjectController: UITableViewController, DimensionCellDelegate {
 
     var project:Project!
     var dimensionCells:[String : DimensionCell] = [:]
@@ -22,11 +22,11 @@ class BaseProjectController: UITableViewController, UIPickerViewDelegate, UIText
         self.clearsSelectionOnViewWillAppear = false
     }
 
-    // Set values for text fields
-    func UpdateText()
-    {
-        
+    // MARK: Dimension Cell Delegate
+    func didChangeDimension() {
+        project.calcYarnRequired()
     }
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -37,8 +37,7 @@ class BaseProjectController: UITableViewController, UIPickerViewDelegate, UIText
         return section == 0 ? 1 : project.dimensions.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->
-            UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "NameImage", for: indexPath) as! NameImageCell
             // Configure the cell...
@@ -58,26 +57,6 @@ class BaseProjectController: UITableViewController, UIPickerViewDelegate, UIText
         tagCells[indexPath.row] = cell
         
         return cell
-    }
-
-    // Update the units and recalculate the yarn needed
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
-    {
-        /*tagCells[pickerView.tag].unit  {
-        case 1:
-            project.gaugeUnits = GaugeUnits(rawValue: row)!
-        case 2:
-            project.yarnNeededUnits = LongLengthUnits(rawValue: row)!
-        case 3:
-            project.ballSizeUnits = LongLengthUnits(rawValue: row)!
-        case 4:
-            project.partialBalls = WholePartial(rawValue: row)!
-        default: break
-        }
-        txtGauge.endEditing(true)
-        txtBallSize.endEditing(true)
-        project.calcYarnRequired()
-        UpdateText()*/
     }
 
 }
--- YarnRequirements/DimensionCell.swift
+++ YarnRequirements/DimensionCell.swift
@@ -8,30 +8,40 @@
 
 import UIKit
 
-class DimensionCell: UITableViewCell, UIPickerViewDataSource {
+protocol DimensionCellDelegate {
+    func didChangeDimension()
+}
+
+class DimensionCell: UITableViewCell, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
 
     @IBOutlet weak var dimensionLabel: UILabel!
     @IBOutlet weak var valueField: UITextField!
     @IBOutlet weak var unitsField: UITextField!
     
-    @IBOutlet weak var picker: UIPickerView!
+    var picker: UIPickerView!
     
     var units:[String]!
     var dimension:DimensionProtocol?
+    var delegate:DimensionCellDelegate!
     
     func didCreate(controller: BaseProjectController, dimension:DimensionProtocol, tag:Int) {
-        valueField.delegate = controller
+        valueField.delegate = self
+        self.delegate = controller
         self.dimension = dimension
-        picker.delegate = controller
+        picker = UIPickerView()
+        picker.delegate = self
         picker.dataSource = self
         picker.tag = tag
+        unitsField.inputView = picker
+        unitsField.delegate = self
         setValues()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+    // MARK: - PickerView delegate
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -40,6 +50,24 @@ class DimensionCell: UITableViewCell, UIPickerViewDataSource {
         return units.count
     }
 
+    func pickerView( _ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return units[row]
+    }
+
+    // Update the units and recalculate the yarn needed
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        unitsField.text = units[row]
+        picker.isHidden = true
+    }
+    
+    // MARK: Text Field Delegate
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        picker.isHidden = false
+        return true
+    }
+    
     func setValues() {
         unitsField.text = units?[dimension!.unitIndex]
         valueField.text = dimension!.valueString
